46bdced7a83018ac33da2587b8f72b816ccf7e55
Clymoplet <jahnnho.moraga@gmail.com>
2023-04-18 17:05:21 -0400

    Se establece build base para proyectos en next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      'api.lorem.space',
+      'cdn.pixabay.com',
+      'placeimg.com',
+      'www.libreriahuequito.com',
+      'thumbs.dreamstime.com',
+      'images.unsplash.com',
+      'ui-avatars.com',
+      'cdna.artstation.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
