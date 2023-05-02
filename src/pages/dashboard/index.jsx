bb57f31387db5f690d7efafa51751b1b4fd24e54
Clymoplet <jahnnho.moraga@gmail.com>
2023-05-01 23:24:33 -0400

    Se traabaja en los contenedores

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -11,14 +11,82 @@ export default function Dashboard() {
             </Link>
           </li>
           <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
-            <span>Sales</span>
+            <span>Inicio</span>
           </li>
         </ul>
         <div className="pt-5">
-          <div className="mb-6 grid gap-6 xl:grid-cols-3">
+          <div className="mb-6 grid gap-4 lg:grid-cols-1 md:grid-cols-2">
             <div className="panel h-full xl:col-span-2">
               <div className="mb-5 flex items-center justify-between dark:text-white-light">
                 <h5 className="text-lg font-semibold">Revenue</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="panel h-full">
+          <div className="mb-5 flex items-center">
+            <h5 className="text-lg font-semibold dark:text-white-light">
+              Sales By Category
+            </h5>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-1">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 1 </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-2">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 2 </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-3">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 3 </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-4">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 4 </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-5">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 5 </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className="mb-6 grid gap-4 lg:grid-cols-4">
+            <div className="panel h-full xl:col-span-2">
+              <div className="mb-5 flex items-center justify-between dark:text-white-light">
+                <h5 className="text-lg font-semibold">Content 6 </h5>
+              </div>
+            </div>
+            <div className="panel h-full">
+              <div className="mb-5 flex items-center lg:grid-cols-1">
+                <h5 className="text-lg font-semibold dark:text-white-light">
+                  Content 6
+                </h5>
               </div>
             </div>
           </div>
