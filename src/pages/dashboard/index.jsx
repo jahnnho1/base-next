a7bf9ac981b6f6561cb21dc986faf111c8f7b50e
Clymoplet <jahnnho.moraga@gmail.com>
2023-05-07 23:34:04 -0400

    Se eliminan borradores, para comenzar a trabajar oficialmente en el dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -15,85 +15,8 @@ export default function Dashboard() {
           </li>
         </ul>
         <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-1 md:grid-cols-2">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Revenue</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="panel h-full">
-          <div className="mb-5 flex items-center">
-            <h5 className="text-lg font-semibold dark:text-white-light">
-              Sales By Category
-            </h5>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-1">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 1 </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-2">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 2 </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-3">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 3 </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
           <div className="mb-6 grid gap-4 lg:grid-cols-4">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 4 </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-5">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 5 </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-3">
-            <div className="panel h-full xl:col-span-2">
-              <div className="mb-5 flex items-center justify-between dark:text-white-light">
-                <h5 className="text-lg font-semibold">Content 6 </h5>
-              </div>
-            </div>
-            <div className="panel h-full">
-              <div className="mb-5 flex items-center lg:grid-cols-1">
-                <h5 className="text-lg font-semibold dark:text-white-light">
-                  Content 6
-                </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-5">
-          <div className="mb-6 grid gap-4 lg:grid-cols-3">
-            <div className="panel h-full">
+            <div className="panel h-full xl:col-span-3">
               <div className="mb-5 flex items-center lg:grid-cols-1">
                 <h5 className="text-lg font-semibold dark:text-white-light">
                   Content 7
@@ -107,7 +30,7 @@ export default function Dashboard() {
                 </h5>
               </div>
             </div>
-            <div className="panel h-full">
+            <div className="panel h-full xl:col-span-3">
               <div className="mb-5 flex items-center lg:grid-cols-1">
                 <h5 className="text-lg font-semibold dark:text-white-light">
                   Content 7
@@ -121,14 +44,7 @@ export default function Dashboard() {
                 </h5>
               </div>
             </div>
-            <div className="panel h-full">
-              <div className="mb-5 flex items-center lg:grid-cols-1">
-                <h5 className="text-lg font-semibold dark:text-white-light">
-                  Content 7
-                </h5>
-              </div>
-            </div>
-            <div className="panel h-full">
+            <div className="panel h-full xl:col-span-4">
               <div className="mb-5 flex items-center lg:grid-cols-1">
                 <h5 className="text-lg font-semibold dark:text-white-light">
                   Content 7
