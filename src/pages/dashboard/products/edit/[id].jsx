c0f087ca4cadf68a53fcc32544d625ce599f1643
Clymoplet <jahnnho.moraga@gmail.com>
2023-05-18 23:58:52 -0400

    se trabaja en el mod de produ

--- src/pages/dashboard/products/edit/[id].jsx
+++ src/pages/dashboard/products/edit/[id].jsx
@@ -1,7 +1,28 @@
-export default function editProduct() {
+import FormProduct from '@components/forms/FormProduct'
+import { useRouter } from 'next/router'
+import { getProduct } from '@services/api/product'
+import { useEffect, useState } from 'react'
+
+export default function EditProduct() {
+  const router = useRouter()
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const { id } = router.query
+    console.log(id)
+    if (id) {
+      getProduct(id)
+        .then((res) => {
+          setProduct(res.props.data)
+        })
+        .catch((err) => console.log(err))
+    }
+  }, [router.isReady, router.query])
+
   return (
     <div>
       <h1>editProduct</h1>
+      <FormProduct product={product} />
     </div>
   )
 }
--- src/services/api/product.js
+++ src/services/api/product.js
@@ -27,4 +27,30 @@ const addProduct = async (body) => {
   }
 }
 
-export { addProduct }
+const getProduct = async (id) => {
+  try {
+    const config = {
+      method: 'GET',
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+    }
+    const response = await fetch(endPoints.products.getProduct(id), config)
+    const data = await response.json()
+    return {
+      props: {
+        data,
+      },
+    }
+  } catch (error) {
+    console.error('Error al obtener los datos:', error)
+    return {
+      props: {
+        data: null,
+      },
+    }
+  }
+}
+
+export { addProduct, getProduct }
