0a1b8534fd67ccb29af2315b04a756786d76023d
Clymoplet <jahnnho.moraga@gmail.com>
2023-05-27 22:10:18 -0400

    Se crea scroll para el responsive de tablas

--- src/components/tables/tableProducts.jsx
+++ src/components/tables/tableProducts.jsx
@@ -68,89 +68,118 @@ export default function TableProducts({ setAlert }) {
   const totalPages = Math.ceil(tableDataProducts.length / productsPerPage)
 
   return (
-    <div className="table-responsive mb-5">
-      <table className="table-hover">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Precio</th>
-            <th>Categoria</th>
-            <th>Stock</th>
-            <th>Descripcion</th>
-            <th>Status</th>
-            <th>Fecha Creacion</th>
-            <th>Ult. Actualizacion</th>
-            <th className="text-center">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentProducts.map((data) => {
-            return (
-              <tr key={data.id}>
-                <td>
-                  <div className="whitespace-nowrap">{data.title}</div>
-                </td>
-                <td className="text-right">${data.price}</td>
-                <td className="text-center">{data.category.name}</td>
-                <td className="text-center">10</td>
-                <td>{data.description.split('...', 10)}</td>
-                <td>
-                  <div
-                    className={`whitespace-nowrap text-center ${
-                      data.status === 'active'
-                        ? 'text-success'
-                        : data.status === 'Pending'
-                        ? 'text-secondary'
-                        : data.status === 'In Progress'
-                        ? 'text-info'
-                        : data.status === 'Canceled'
-                        ? 'text-danger'
-                        : 'text-success'
-                    }`}
-                  >
-                    {data?.status || 'active'}
-                  </div>
-                </td>
-                <td className="text-center">{formatDate(data.creationAt)}</td>
-                <td className="text-center">{formatDate(data.updatedAt)}</td>
-                <td className="text-center">
-                  <TrashIcon
-                    className="h-6 w-6 text-red-500 inline cursor-pointer"
-                    onClick={() => handleDeleteProduct(data.id)}
-                  />
-                  <CogIcon
-                    className="h-6 w-6 text-black-500 inline cursor-pointer"
-                    onClick={() => handleEditProduct(data)}
-                  />
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-      <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            className={`pagination-button ${
-              currentPage === index + 1 ? 'active' : ''
-            }`}
-            onClick={() => handlePageChange(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
+    <div className="grid grid-cols-1">
+      <div className="table-responsive col-span-1 mb-5">
+        <table className="table-hover">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Categoria</th>
+              <th>Stock</th>
+              <th>Descripcion</th>
+              <th>Status</th>
+              <th>Fecha Creacion</th>
+              <th>Ult. Actualizacion</th>
+              <th className="text-center">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentProducts.map((data) => {
+              return (
+                <tr key={data.id}>
+                  <td>
+                    <div className="whitespace-nowrap">{data.title}</div>
+                  </td>
+                  <td className="text-right">${data.price}</td>
+                  <td className="text-center">{data.category.name}</td>
+                  <td className="text-center">10</td>
+                  <td>{data.description.substring(0, 10) + '...'} </td>
+                  <td>
+                    <div
+                      className={`whitespace-nowrap text-center ${
+                        data.status === 'active'
+                          ? 'text-success'
+                          : data.status === 'Pending'
+                          ? 'text-secondary'
+                          : data.status === 'In Progress'
+                          ? 'text-info'
+                          : data.status === 'Canceled'
+                          ? 'text-danger'
+                          : 'text-success'
+                      }`}
+                    >
+                      {data?.status || 'active'}
+                    </div>
+                  </td>
+                  <td className="text-center">{formatDate(data.creationAt)}</td>
+                  <td className="text-center">{formatDate(data.updatedAt)}</td>
+                  <td className="text-center">
+                    <TrashIcon
+                      className="h-6 w-6 text-red-500 inline cursor-pointer"
+                      onClick={() => handleDeleteProduct(data.id)}
+                    />
+                    <CogIcon
+                      className="h-6 w-6 text-black-500 inline cursor-pointer"
+                      onClick={() => handleEditProduct(data)}
+                    />
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        {open ? (
+          <Modal open={open} setOpen={setOpen}>
+            <FormProduct
+              setAlert={setAlert}
+              setOpen={setOpen}
+              title="Editar producto"
+              product={productEdit}
+            />
+          </Modal>
+        ) : null}
       </div>
-      {open ? (
-        <Modal open={open} setOpen={setOpen}>
-          <FormProduct
-            setAlert={setAlert}
-            setOpen={setOpen}
-            title="Editar producto"
-            product={productEdit}
-          />
-        </Modal>
-      ) : null}
+      <div className="col-span-1 table-responsive py-5">
+        <nav aria-label="Page navigation">
+          <ul className="inline-flex -space-x-px">
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Previous
+              </a>
+            </li>
+            <li>
+              {Array.from({ length: totalPages }, (_, index) => (
+                <a
+                  href="#"
+                  className={
+                    currentPage !== index + 1
+                      ? `px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`
+                      : 'px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                  }
+                  key={index}
+                  onClick={() => handlePageChange(index + 1)}
+                  aria-current={currentPage === index + 1 ? 'page' : undefined}
+                >
+                  {index + 1}
+                </a>
+              ))}
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
