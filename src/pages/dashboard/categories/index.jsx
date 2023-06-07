d5499de0fd3f5481435d8cfb588b7fe2e9410daa
Clymo <jahnnho.moraga@gmail.com>
2023-06-06 23:39:17 -0400

    Se comienza a trabajar con las  categorias

--- src/pages/dashboard/categories/index.jsx
+++ src/pages/dashboard/categories/index.jsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+
 export default function HomeCategories() {
   return (
-    <div>
-      <h1>HomeCategories</h1>
-    </div>
+    <>
+      <ul className="flex space-x-2 rtl:space-x-reverse">
+        <li>
+          <Link href="/dashboard" className="text-primary hover:underline">
+            Inicio Dashboard
+          </Link>
+        </li>
+        <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
+          <span>Categorias</span>
+        </li>
+      </ul>
+    </>
   )
 }
