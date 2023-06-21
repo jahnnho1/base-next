58042183ef7bd23d89853a65c18abd41629756bb
Clymo <jahnnho.moraga@gmail.com>
2023-06-20 23:40:57 -0400

    se sube

--- src/pages/dashboard/categories/index.jsx
+++ src/pages/dashboard/categories/index.jsx
@@ -16,8 +16,8 @@ export default function HomeCategories() {
       <ul className="flex space-x-2 rtl:space-x-reverse">
         <li>
           <Link href="/dashboard" className="text-primary hover:underline">
-            Inicio Dashboard
-          </Link>
+            Inicio Dashboard test
+          </Link> 
         </li>
         <li className="before:content-['/'] ltr:before:mr-2 rtl:before:ml-2">
           <span>Categorias</span>
